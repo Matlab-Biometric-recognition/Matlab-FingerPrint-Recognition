b7af9e7cf3abc22360d00e9ae05af941d11e9648
Hamdi Boukamcha <contact@matlab-recognition-code.com>
2018-12-28 14:04:05 +0100

    Add files via upload

--- FingerPrint/sourcecode.m
+++ FingerPrint/sourcecode.m
@@ -1,12 +1,10 @@
-% Plase Donate to obtain the complete source code please visit
-% http://matlab-recognition-code.com/fingerprint-recognition-system-full-source-code/
-%For further question please email me hamdouchhd@hotmail.fr
+% -----Project : Fingerprint Recognition Project-----
+%----------------------------------------------------
+% Please Donate to obtain the Full source code.
+% Link : https://matlab-recognition-code.com/matlab-fingerprint-recognition-system-full-source-code/
 % 
-% Hamdi Boukamcha
-% Tunisie 
-% 4081 Sousse
-% 
-% mobile +21650674269
-% email hamdouchhd@hotmail.fr
-% website http://matlab-recognition-code.com
+% Developer: PhD, Hamdi Boukamcha, Faculty of Science of Monastir, Tunisia.
+% Email: Contact@matlab-recognition-code.com
+% WhatsAPP: +21650674269
+% Website: https://Matlab-Recognition-Code.com
 %
